0c96497b83882ce8fe4b4ad9a01b2033cf0e7fa4
QKKQQK <zhouqiankang@hotmail.com>
2017-10-18 18:42:04 -0500

    Test datastore get

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -3,13 +3,18 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 var secret = require('../secret.json')
 
+const Datastore = require('@google-cloud/datastore');
+const datastore = Datastore({
+  projectId: secret.data.projectId
+});
+const kind = 'User_V1';
+
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
 // called everytime user_controller is called
 router.use(function timeLog (req, res, next) {
   console.log('In User Controller @ Time: ', Date.now());
-  console.log(secret.data.projectID);
   next();
 });
 
@@ -17,7 +22,6 @@ router.use(function timeLog (req, res, next) {
 router.route('/')
 	.get(function(req, res) {
 		//TODO Access Datastore
-
 		res.status(200);
 		res.json({ message: 'Testing GET /api/users'});
 	})
@@ -28,5 +32,19 @@ router.route('/')
 		res.status(200);
 		res.json({ message: 'Testing POST /api/users'});
 	});
+
+router.route('/:id')
+	.get(function(req, res) {
+		//TODO Access Datastore
+		// datastore.get(req.params.id, function(err, entity) {
+		// 	if (err) {
+		// 		res.status(500)
+		// 		res.json({ message : "Error" })
+		// 	} else {
+		// 		res.status(200);
+		// 		res.json(entity);
+		// 	}
+		// });
+	})
 
 module.exports = router;
