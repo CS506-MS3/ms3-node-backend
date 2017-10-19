ad2653d90752f91f0fd8b64510c80a666ef24549
Kyle <zhouqiankang@hotmail.com>
2017-10-19 02:32:56 +0000

    POST /api/users/ now check for missing parameters

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -34,24 +34,54 @@ router.route('/')
 
 	.post(function(req, res) {
 		//TODO Access & Update Datastore
-		var key = datastore.key(['User_V1']);
-
-		var data = {
+		var valid = true;
+		var key = "";
+		var data = "";
+		try {
+			if (
+				req.body.bid === undefined || req.body.wishlist === undefined ||
+				req.body.access === undefined || req.body.phone === undefined ||
+				req.body.listing === undefined || req.body.stripe_id === undefined ||
+				req.body.active === undefined || req.body.email === undefined ||
+				req.body.password_hash === undefined || req.body.notification === undefined
+			) {
+				throw 'Missing params';
+			}
+			key = datastore.key(['User_V1']);
+			data = {
+				bid : req.body.bid,
+				wishlist : req.body.wishlist,
+				access : req.body.access,
+				phone : req.body.phone,
+				listing : req.body.listing,
+				stripe_id : req.body.stripe_id,
+				active : req.body.active,
+				email : req.body.email,
+				password_hash : req.body.password_hash,
+				notification : req.body.notification
+			};
+		} catch (err){
+			valid = false;
+			res.status(400);
+                        console.error("Error:", err);
+			res.json({ message: 'Error'});
+		}
 		
-		};
-
-		datastore.save({
-		  	key: key,
- 			data: data
-		}, function(err) {
-  			if (!err) {
-    				// Record saved successfully.
- 			}
-		}).catch((err) => {
-                        console.error('ERROR:', err);
-                        res.status(500);
-                        res.json({ message: "Error" })
-                });
+		if (valid) {
+			datastore.save({
+			  	key: key,
+ 				data: data
+			}, function(err) {
+  				if (!err) {
+    					res.status(201);
+					res.json({ message: "Created" });
+	 			} else {
+					console.error("Create Error:", err);
+					res.status(500);
+					res.json({ message: "Error" });
+				}
+			});
+		}
 	});
 
 module.exports = router;
