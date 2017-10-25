907853ec5e01528e35533e25a79f7d0ce6f1141a
QKKQQK <zhouqiankang@hotmail.com>
2017-10-24 20:10:52 -0500

    Modify return statement

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -19,13 +19,13 @@ router.route('/')
                                	const users = results[0];
                             	res.status(200);
 								res.json(users);
-								next();
+								return next();
                        	})
 						.catch((err) => {
 			 				console.error('ERROR:', err);
 			                    res.status(500);
 			                    res.json({ message: "Error" });
-			                    next();
+			                    return next();
 						});
  	})
 
@@ -48,13 +48,13 @@ router.route('/')
                                	if (users.length != 0) {
                                		res.status(409);
                                		res.json({ message: "User Already Exists" });
-                               		next();
+                               		return next();
                                	}
                        	})
 						.catch((err) => {
                         		res.status(500);
                        			res.json({ message: "Error" });
-                       			next();
+                       			return next();
 						});
 
 			key = datastore.key(['User_V1']);
@@ -73,7 +73,7 @@ router.route('/')
 		} catch (err){
 			res.status(400);
 			res.json({ message: 'Invalid Syntax'});
-			next();
+			return next();
 		}
 		datastore.save({
 			  	key: key,
@@ -83,11 +83,11 @@ router.route('/')
   				if (!err) {
     				res.status(201);
 					res.json({ message: "Created" });
-					next();
+					return next();
 	 			} else {
 					res.status(500);
 					res.json({ message: "Error" });
-					next();
+					return next();
 				}
 		});
 	});
