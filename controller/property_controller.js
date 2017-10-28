2bb2fd098db90e59b611ab54092942a2adc2c472
QKKQQK <zhouqiankang@hotmail.com>
2017-10-28 13:17:08 -0500

    Add property controller file

---
+++ controller/property_controller.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+var secret = require('../secret.json')
+
+const Datastore = require('@google-cloud/datastore');
+const datastore = Datastore();
+
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
+
+router.use(function timeLog (req, res, next) {
+  console.log('In Properties Controller @ Time: ', Date.now());
+  next();
+});
