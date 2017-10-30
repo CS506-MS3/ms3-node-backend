385b47b0ea9cfc13090fb37ecd4ff1a142605846
QKKQQK <zhouqiankang@hotmail.com>
2017-10-30 12:41:18 -0500

    Bug Fix: missing response in sign in query err

--- controller/auth_controller.js
+++ controller/auth_controller.js
@@ -18,127 +18,128 @@ router.use(function timeLog (req, res, next) {
 
 router.route('/')
 
-		.post(function(req, res, next){
-			if (req.body.email === undefined || req.body.password === undefined){ 
-				res.status(400);
-				res.json({ message: "Malformed Request" });
-			} else {
-				next();
-			}
-		}, function(req, res, next) {
-			try {
-				const query = datastore.createQuery('User_V1').filter('email', '=', req.body.email);
-				datastore.runQuery(query, function(err, entities) {
-					if (err) {
+	.post(function(req, res, next){
+		if (req.body.email === undefined || req.body.password === undefined){ 
+			res.status(400);
+			res.json({ message: "Malformed Request" });
+		} else {
+			next();
+		}
+	}, function(req, res, next) {
+		try {
+			const query = datastore.createQuery('User_V1').filter('email', '=', req.body.email);
+			datastore.runQuery(query, function(err, entities) {
+				if (err) {
+					console.error(err);
+					res.status(500);
+					res.json({ message: "Internal Server Error" });
+				} else {
+					try {
+                    	var password_hash = crypto.createHmac('sha256', secret.password_secret)
+							.update(req.body.password)
+							.digest('hex');
+						if (entities.length == 0) {
+			                res.status(401);
+			                res.json({ message: "Invalid Email/Password Combo" });
+			            } else {
+			                var user_data = entities[0];
+			                var user_key = entities[0][datastore.KEY];
+			                if (user_data.active === false){
+			                    res.status(403);
+			                    res.json({ message: "Inactive account" });
+			                } else if (user_data.password_hash !== password_hash){
+			                    res.status(401);
+			                    res.json({ message: "Invalid Email/Password Combo" });
+			                } else {
+			                	res.locals.id = user_key.id;
+			                	delete user_data["password_hash"];
+								delete user_data["stripe_id"];
+								delete user_data["active"];
+								res.locals.user_data = user_data;
+			                    next();	
+			                }
+			            }
+					} catch (err) {
 						console.error(err);
 						res.status(500);
 						res.json({ message: "Internal Server Error" });
-					} else {
-						try {
-	                    	var password_hash = crypto.createHmac('sha256', secret.password_secret)
-								.update(req.body.password)
-								.digest('hex');
-							if (entities.length == 0) {
-				                res.status(401);
-				                res.json({ message: "Invalid Email/Password Combo" });
-				            } else {
-				                var user_data = entities[0];
-				                var user_key = entities[0][datastore.KEY];
-				                if (user_data.active === false){
-				                    res.status(403);
-				                    res.json({ message: "Inactive account" });
-				                } else if (user_data.password_hash !== password_hash){
-				                    res.status(401);
-				                    res.json({ message: "Invalid Email/Password Combo" });
-				                } else {
-				                	res.locals.id = user_key.id;
-				                	delete user_data["password_hash"];
-									delete user_data["stripe_id"];
-									delete user_data["active"];
-									res.locals.user_data = user_data;
-				                    next();	
-				                }
-				            }
-						} catch (err){
-							console.error(err);
-							res.status(500);
-							res.json({ message: "Internal Server Error" });
-						}
 					}
-				});
-			} catch (err){
-				console.error("Create Query Error");
-				res.status(500);
-				res.json({ message: "Internal Server Error" });
-			}
-		}, function(req, res){
-			try {
-				var token = jwt.sign({
-					data: {
-						id : res.locals.id,
-						email : res.locals.user_data.email,
-						type : 'user'
-					}
-				}, secret.token_secret, { expiresIn: '14d' });
-
-				res.status(200);
-				res.json({
-					token: token,
-					data: res.locals.user_data
-				});
-			} catch (err) {
-				console.error(err);
-				res.status(500);
-				res.json({ message: "Internal Server Error" });
-			}
-		})
-
-		.delete(function(req, res, next){
-				try {
-					var token = req.get('token')
-					var decoded = jwt.verify(token, secret.token_secret);
-					res.locals.token = token;
-					res.locals.decoded = decoded;
-					next();
-				} catch (err) {
-					console.error("Invalid Token");
-					res.status(204).send();
-				}
-		}, function(req, res, next){
-			try {
-				const query = datastore.createQuery('Token_Blacklist_V1').filter('token', '=', res.locals.token);
-				datastore.runQuery(query, function(err, entities) {
-					if (err) {
-						console.error("Run Query Error");
-					} else {
-						if (entities.length == 0) {
-			                res.locals.token_key = datastore.key(['Token_Blacklist_V1']);
-							res.locals.token_data = {
-								token : res.locals.token,
-								exp : res.locals.decoded.exp
-							};
-							next();
-		                } else {
-		                	console.error("Token already blacklisted");
-		                	res.status(204).send();
-	                    }
-					}
-				});
-			} catch (err) {
-				console.error("Create Query Error");
-				res.status(204).send();
-			}
-		}, function(req, res){
-			datastore.save({
-				key: res.locals.token_key,
-				data: res.locals.token_data
-			}, function(err) {
-				if (err) {
-					console.error(err);
 				}
 			});
+		} catch (err) {
+			console.error("Create Query Error");
+			res.status(500);
+			res.json({ message: "Internal Server Error" });
+		}
+	}, function(req, res){
+		try {
+			var token = jwt.sign({
+				data: {
+					id : res.locals.id,
+					email : res.locals.user_data.email,
+					type : 'user'
+				}
+			}, secret.token_secret, { expiresIn: '14d' });
+
+			res.status(200);
+			res.json({
+				token: token,
+				data: res.locals.user_data
+			});
+		} catch (err) {
+			console.error(err);
+			res.status(500);
+			res.json({ message: "Internal Server Error" });
+		}
+	})
+
+	.delete(function(req, res, next){
+		try {
+			var token = req.get('token')
+			var decoded = jwt.verify(token, secret.token_secret);
+			res.locals.token = token;
+			res.locals.decoded = decoded;
+			next();
+		} catch (err) {
+			console.error("Invalid Token");
 			res.status(204).send();
+		}
+	}, function(req, res, next){
+		try {
+			const query = datastore.createQuery('Token_Blacklist_V1').filter('token', '=', res.locals.token);
+			datastore.runQuery(query, function(err, entities) {
+				if (err) {
+					console.error("Run Query Error");
+					res.status(204).send();
+				} else {
+					if (entities.length == 0) {
+		                res.locals.token_key = datastore.key(['Token_Blacklist_V1']);
+						res.locals.token_data = {
+							token : res.locals.token,
+							exp : res.locals.decoded.exp
+						};
+						next();
+	                } else {
+	                	console.error("Token already blacklisted");
+	                	res.status(204).send();
+                    }
+				}
+			});
+		} catch (err) {
+			console.error("Create Query Error");
+			res.status(204).send();
+		}
+	}, function(req, res){
+		datastore.save({
+			key: res.locals.token_key,
+			data: res.locals.token_data
+		}, function(err) {
+			if (err) {
+				console.error(err);
+			}
 		});
+		res.status(204).send();
+	});
 
 
 module.exports = router;
