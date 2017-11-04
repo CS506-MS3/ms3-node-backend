6ef82c59da13f399cb74d5aa0d8a6c8d304b593b
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-11-04 10:33:05 -0500

    feat(blacklist): add get

--- middlewares/blacklist.middleware.js
+++ middlewares/blacklist.middleware.js
@@ -4,8 +4,24 @@ module.exports = (function () {
     const ENTITY_KEY = 'Email_Blacklist_Dev';
 
     function getList(req, res) {
+        const query = datastore.createQuery(ENTITY_KEY);
 
+        datastore.runQuery(query, function (error, entities) {
+           if (error) {
+
+               errorResponse.send(res, 500, 'Internal Server Error', error);
+           } else if (entities.length === 0) {
+
+               res.status(200).json([]);
+           } else {
+
+               // Skip pagination for now.
+               res.status(200).json(entities);
+           }
+        });
     }
 
-    return {};
+    return {
+        getList: getList
+    };
 })();
