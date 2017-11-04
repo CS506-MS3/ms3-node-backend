0e67c301601feca537a330243b3f83ed92c499d0
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-11-04 17:57:07 -0500

    feat(config): add configs

---
+++ configs/roles.constants.js
@@ -0,0 +1,7 @@
+const ROLES = {
+    USER: 'user',
+    EMPLOYEE: 'employee',
+    SUPER_ADMIN: 'superadmin'
+};
+
+module.exports = ROLES;
---
+++ configs/token.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    DEFAULT_EXPIRY: '14d',
+    ACTIVATION_LINK_EXPIRY: '1h'
+};
