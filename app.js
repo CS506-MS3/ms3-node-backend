3dca97845343f3b06767cac96a3d224607e9984d
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-11-04 18:01:53 -0500

    refact(app.js): refactor to singleton + factory pattern

--- app.js
+++ app.js
@@ -1,51 +1,101 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var router = express.Router();
+/* Core Dependencies */
+const express = require('express');
+const bodyParser = require('body-parser');
 
-require('super-admin-creator')();
+/* Other 3rd Party Dependencies */
+const secret = require('../secret/secret.json');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+const nodemailer = require('nodemailer');
 
+/* Import Core Modules */
+const permissions = require('./core/permissions');
+const errorResponseService = require('./core/error-response');
+const Tokenizer = require('./core/tokenizer.service');
+const Mailer = require('./core/mail.service');
+
+/* Import Middlewares */
+const AuthMiddleware = require('./core/auth');
+const EmployeesMiddleware = require('./middlewares/employees.middleware');
+const UsersMiddleware = require('./middlewares/users.middleware');
+const BlacklistMiddleware = require('./middlewares/blacklist.middleware');
+
+/* Import Controllers */
+const EmployeesController = require('./controller/employees.controller');
+const EmployeeAuthController = require('./controller/employee-auth.controller');
+const BlacklistController = require('./controller/blacklist.controller');
+const UserController = require('./controller/user.controller');
+
+/* Import Config Constants */
+const CONFIG = {
+    WEB_URL: 'https://ms3-web.firebaseapp.com',
+    TOKEN_CONFIG: require('./configs/token.config'),
+    ROLES: require('./configs/roles.constants'),
+    ENTITY_KEYS: require('./configs/entity-keys.constants')
+};
+// TODO: CONFIG & ENV should be combined
+/* Import Env */
+const ENV = require('./environments/environment');
+
+/* Set-up Scripts */
+require('./core/super-admin-creator')();
+
+/* Initialize Application */
+const app = express();
+const router = express.Router();
+
+/* Global Route Settings */
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
+    next();
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// TODO /api/users
-var userController = require('./controller/user_controller'); // import user controller module (user_controller.js) from controller dir
-var authController = require('./controller/auth_controller');
-var reactivateController = require('./controller/reactivate_controller');
-var activateController = require('./controller/activate_controller');
+/* Connect DB */
+const datastore = ENV.connectToDatastore();
 
+/* Initialize Core Services */
+const tokenizer = new Tokenizer(jwt, secret, CONFIG);
+const mailer = new Mailer(nodemailer, tokenizer, secret, CONFIG);
 
-router.use('/users', userController); // mount /api/users to user controller module
+/* Initialize Middleware */
+const auth = AuthMiddleware(datastore, errorResponseService, secret, jwt, CONFIG);
+const employees = EmployeesMiddleware(datastore, errorResponseService, secret, crypto, CONFIG);
+const users = UsersMiddleware(datastore, errorResponseService, secret, crypto, CONFIG);
+const blacklist = BlacklistMiddleware(datastore, errorResponseService, CONFIG);
+
+/* Initialize Controllers */
+const employeesController = EmployeesController(express, bodyParser, permissions, auth, employees, CONFIG);
+const employeeAuthController = EmployeeAuthController(express, bodyParser, auth, employees);
+const blacklistController = BlacklistController(express, bodyParser, permissions, auth, blacklist, CONFIG);
+const userController = UserController(express, bodyParser, permissions, mailer, auth, users);
+
+/* Add Routes */
+router.use('/employees', employeesController);
+router.use('/employee-auth', employeeAuthController);
+router.use('/blacklist', blacklistController);
+router.use('/users', userController);
+
+// TODO: REFACTOR CONTROLLERS
+const authController = require('./controller/auth_controller');
+const reactivateController = require('./controller/reactivate_controller');
+const activateController = require('./controller/activate_controller');
 router.use('/auth', authController);
 router.use('/reactivate', reactivateController);
 router.use('/activate', activateController);
 
-
-const employeeAuthController = require('./controller/employee-auth.controller');
-const employeesController = require('/controller/employees.controller');
-
-router.use('/employee-auth', employeeAuthController);
-router.use('/employees', employeesController);
-
-// TODO /api/authenticate
 // TODO /api/access
-// TODO /api/employee
 // TODO /api/
-// TODO /api/blacklist
 
 app.get('/', function(req, res){
 	res.send('Hello');
 });
 
-app.use('/api', router)
-
-
-app.listen(3000)
+/* Run */
+app.use('/api', router);
+app.listen(3000);
 
