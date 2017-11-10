822536f1fd34a9567d1fd6c69551df57db2837e5
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-11-10 14:51:34 -0600

    feat(properties): add options GET api

--- configs/entity-keys.constants.js
+++ configs/entity-keys.constants.js
@@ -4,7 +4,8 @@ const ENTITY_KEYS = {
     TOKEN_BLACKLIST: 'Token_Blacklist_V1',
     EMAIL_BLACKLIST: 'Email_Blacklist_V1',
     PROPERTIES: 'Listings_V1',
-    PRICINGS: 'Pricings_1.0.0'
+    PRICINGS: 'Pricings_1.0.0',
+    PROPERTY_OPTIONS: 'PropertyOptions_1.0.0'
 };
 
 module.exports = ENTITY_KEYS;
--- middlewares/properties.middleware.js
+++ middlewares/properties.middleware.js
@@ -1,4 +1,51 @@
+function propertiesMiddleware(datastore, errorResponse, CONFIG) {
+    'use strict';
+
+    const ENTITY_KEY = CONFIG.ENTITY_KEYS.PROPERTIES;
+    const OPTIONS_ENTITY_KEY = CONFIG.ENTITY_KEYS.PROPERTY_OPTIONS;
+
+    return {
+        create,
+        getOptions
+    };
+
+    function create(req, res, next) {
+
+    }
+
+    function getOptions(req, res) {
+        const query = datastore.createQuery(OPTIONS_ENTITY_KEY);
+
+        datastore.runQuery(query)
+            .then((response) => {
+                const entities = response[0].map((entity) => {
+                    return {
+                        id: entity[datastore.KEY].id,
+                        type: entity.type,
+                        alias: entity.alias
+                    };
+                });
+
+                res.status(200).json(entities);
+            })
+            .catch((error) => {
+
+                errorResponse.send(res, 500, 'Internal Server Error', error);
+            });
+    }
+}
+
+module.exports = propertiesMiddleware;
+
 /**
+ *  PropertyOptions Entity
+ *
+ *  __key__=id:numeric
+ *  {
+ *      type: AMENITIES | HOUSE_RULES | PETS,
+ *      alias: string
+ *  }
+ *
  *  Properties Entity
  *
  *  __key__=id:numeric
@@ -22,9 +69,7 @@
  *      *startDate: datetime,
  *      *duration: number,
  *      *price: number,
- *      amenities: Amenities.__key__[],
- *      pets: Pets.__key__[],
- *      houseRules: HouseRules.__key__[],
+ *      options: PropertyOptions.__key__[],
  *      imageUrls: string[],
  *      *owner: User.__key__,
  *      *createdAt: datetime,
